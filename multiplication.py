bf8261b0732c1026984debc5c1bc5e63c9703972
Harry2166 <harry2166.hq@gmail.com>
2025-03-03 23:00:04 +0800

    refactor: create a MultiplicationAlgorithm interface so we can possibly expand our examples to more algorithms in the future

--- multiplication.py
+++ multiplication.py
@@ -1,39 +1,53 @@
 # algorithms are from this youtube video: https://www.youtube.com/watch?v=AMl6EJHfUWo&t=1s 
 
-def naive_multi(num1: int, num2: int):
-    if num1 < 2 and num2 < 2: return num1 & num2
-    n = max(num1.bit_length(), num2.bit_length())
-    mid = n >> 1
+import time
+from typing import Protocol
 
-    a,b = num1 >> mid, num1 & ((1 << mid) - 1)
-    c,d = num2 >> mid, num2 & ((1 << mid) - 1)
+class MultiplicationAlgorithm(Protocol):
+    def get_name(self) -> str:
+        ...
+    def multiply(self, num1: int, num2: int) -> int:
+        ...
 
-    ac = naive_multi(a,c)
-    bd = naive_multi(b,d)
-    ad = naive_multi(a,d)
-    bc = naive_multi(b,c)
+class NaiveMultiplication:
 
-    ad_plus_bc = ad + bc
+    def get_name(self) -> str:
+        return "naive-multiplication"
 
-    return (ac << (mid << 1)) + (ad_plus_bc << mid) + bd
+    def multiply(self, num1: int, num2: int) -> int:
+        if num1 < 2 and num2 < 2: return num1 & num2
+        n = max(num1.bit_length(), num2.bit_length())
+        mid = n >> 1
 
+        a,b = num1 >> mid, num1 & ((1 << mid) - 1)
+        c,d = num2 >> mid, num2 & ((1 << mid) - 1)
 
-def karatsuba(num1: int, num2: int):
-    if num1 < 2 and num2 < 2: return num1 & num2
-    n = max(num1.bit_length(), num2.bit_length())
-    mid = n >> 1
+        ac = self.multiply(a,c)
+        bd = self.multiply(b,d)
+        ad = self.multiply(a,d)
+        bc = self.multiply(b,c)
 
-    a,b = num1 >> mid, num1 & ((1 << mid) - 1)
-    c,d = num2 >> mid, num2 & ((1 << mid) - 1)
+        ad_plus_bc = ad + bc
 
-    ac = karatsuba(a,c)
-    bd = karatsuba(b,d)
+        return (ac << (mid << 1)) + (ad_plus_bc << mid) + bd
 
-    ad_plus_bc = karatsuba(a+b,c+d) - ac - bd
+class KaratsubaAlgorithm:
+    
+    def get_name(self) -> str:
+        return "karatsuba"
 
-    return (ac << (mid << 1)) + (ad_plus_bc << mid) + bd
+    def multiply(self, num1: int, num2: int) -> int:
+        if num1 < 2 and num2 < 2: return num1 & num2
+        n = max(num1.bit_length(), num2.bit_length())
+        mid = n >> 1
 
-if __name__ == "__main__":
-    print(naive_multi(12312831627491231, 1283712319823) == (12312831627491231 * 1283712319823))
-    print(karatsuba(12312831627491231, 1283712319823)== (12312831627491231 * 1283712319823)) 
+        a,b = num1 >> mid, num1 & ((1 << mid) - 1)
+        c,d = num2 >> mid, num2 & ((1 << mid) - 1)
+
+        ac = self.multiply(a,c)
+        bd = self.multiply(b,d)
+
+        ad_plus_bc = self.multiply(a+b,c+d) - ac - bd
+
+        return (ac << (mid << 1)) + (ad_plus_bc << mid) + bd
 
